f19d52f5403621c392ad9263e0d4b5d8823833f4
Rhuan12 <rhuan.m.filgueira@gmail.com>
2025-10-23 11:55:56 -0300

    adicionar mudanças

--- real-estate-website/src/app/layout.tsx
+++ real-estate-website/src/app/layout.tsx
@@ -10,8 +10,6 @@ export const metadata: Metadata = {
   keywords: ['real estate', 'properties', 'houses', 'apartments', 'premium homes', 'luxury real estate'],
   authors: [{ name: 'McSilva & Wiggit' }],
   
-  // ✨ Ícones - Next.js detecta automaticamente os arquivos em src/app/
-  // Mas você pode declarar explicitamente se preferir
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
@@ -36,7 +34,6 @@ export const metadata: Metadata = {
     ],
   },
   
-  // Para Twitter/X
   twitter: {
     card: 'summary',
     title: 'McSilva & Wiggit - Premium Real Estate',
@@ -52,7 +49,55 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PP72K9M6');`
+        }} />
+        {/* End Google Tag Manager */}
+
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2082436685850546');
+fbq('track', 'PageView');
+          `
+        }} />
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2082436685850546&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={`${inter.className} bg-background-primary text-text-primary antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PP72K9M6"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <div className="min-h-screen flex flex-col">
           {children}
         </div>
